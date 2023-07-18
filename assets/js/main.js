95bdfbe4f4c8b72affea99df4978976e3866ea89
_userNull <122583299+sendNull@users.noreply.github.com>
2023-07-18 10:57:45 +0700

    add: blur overlay and responsive feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,8 @@ window.addEventListener("scroll", () => {
 let reviewCarousel = new Splide(".review-carousel", {
     type   : 'loop',
     padding: '20%',
-    height: '100%',
+    // height: '100%',
+
     autoplay: true,
     drag: false,
     focus: 'center',
@@ -48,6 +49,7 @@ let reviewCarousel = new Splide(".review-carousel", {
     breakpoints:{
         500: {
             padding: '0',
+            
             arrows: false,
             drag: true,
         }
@@ -55,3 +57,53 @@ let reviewCarousel = new Splide(".review-carousel", {
 
 });
     reviewCarousel.mount();
+
+// $('.hamburger').click(function() {
+//   $( this ).toggleClass( "active" );
+// });
+
+
+var hamburger = document.querySelector(".hamburger"),
+    menuLinks = document.querySelectorAll(".menu-links"),
+    menu = document.querySelector(".menu"),
+    html = document.documentElement,
+    blurOverlay = document.querySelector(".blur-overlay");
+
+hamburger.onclick = function(){
+  hamburger.classList.toggle("active");
+  blurOverlay.classList.toggle("active");
+  menu.classList.toggle("active");
+  navbar.classList.toggle("active-menu");
+  html.classList.toggle("hidden");
+  menuLinks.forEach(function(link, index){
+    setTimeout(() => {
+        link.classList.toggle("active");
+    }, index * 100)
+  });
+}
+
+menu.addEventListener("click", function(event) {
+  if (event.target.classList.contains("menu-links")) {
+    menu.classList.remove("active");
+    navbar.classList.remove("active-menu");
+    hamburger.classList.remove("active");
+    html.classList.remove("hidden");
+    blurOverlay.classList.remove("active");
+    menuLinks.forEach(function(link) {
+      link.classList.remove("active");
+    });
+  }
+});
+
+blurOverlay.addEventListener("click", function(event) {
+  if (event.target.classList.contains("active")) {
+    menu.classList.remove("active");
+    navbar.classList.remove("active-menu");
+    hamburger.classList.remove("active");
+    html.classList.remove("hidden");
+    blurOverlay.classList.remove("active");
+    menuLinks.forEach(function(link) {
+      link.classList.remove("active");
+    });
+  }
+});
